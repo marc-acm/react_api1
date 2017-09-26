3282f6db803babb6646c367f1c794badc87c9efd
marc-acm <marc.macariola@yahoo.com>
2017-09-26 10:05:45 +0800

    update Apps

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import  Api from './api.js';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+     
+      
+        <Api />
+
+
+
+      </div>
+
+       
+
+
+    );
+  }
+}
+
+export default App;
